27839eeadd21a8dfc8aa27a85dfd29096061deee
Tulio Aoki <tulioaoki@gmail.com>
2019-11-18 00:00:13 -0300

    Updates

---
+++ precache-manifest.b37cf7f53b1c2584b8719bf24ec2ce51.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f6500f8c9459b21b71edc3e9965a535",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d02edf849281abd4abf",
+    "url": "/static/css/main.4ee3ac23.chunk.css"
+  },
+  {
+    "revision": "7df7561a333a81af715c",
+    "url": "/static/js/2.0024bac8.chunk.js"
+  },
+  {
+    "revision": "0d02edf849281abd4abf",
+    "url": "/static/js/main.725602f9.chunk.js"
+  },
+  {
+    "revision": "9e479d9176aa29ba14a1",
+    "url": "/static/js/runtime-main.b45374c8.js"
+  },
+  {
+    "revision": "4807a6d44a93d8402b89730822e1ffb1",
+    "url": "/static/media/barbg.4807a6d4.jpg"
+  },
+  {
+    "revision": "271f6bc852a0f704fd45d8a162508067",
+    "url": "/static/media/gast.271f6bc8.png"
+  },
+  {
+    "revision": "74f76eb2311db2bc96777762d56d29ca",
+    "url": "/static/media/petrolindaV4.1.74f76eb2.png"
+  },
+  {
+    "revision": "f9a41599a927a25774c3088b21a0dd57",
+    "url": "/static/media/petrolindaV4.f9a41599.png"
+  }
+]);
